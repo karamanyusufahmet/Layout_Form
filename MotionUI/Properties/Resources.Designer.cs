e6c189b38363a3b678f95f320c866a496cb794ae
Ahmet Yusuf Karaman <90053607+karamanyusufahmet@users.noreply.github.com>
2024-12-19 11:24:52 +0300

    Update Resources.Designer.cs
    Test

--- MotionUI/Properties/Resources.Designer.cs
+++ MotionUI/Properties/Resources.Designer.cs
@@ -79,5 +79,20 @@ namespace MotionUI.Properties
                 resourceCulture = value;
             }
         }
+
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
+                return resourceCulture;
+            }
+            set
+            {
+                resourceCulture = value;
+            }
+        }
+
+        
     }
 }
