7b820a992dd0c17e9ddbe2bc48e1f3bd5befbe26
Ahmet Yusuf Karaman <90053607+karamanyusufahmet@users.noreply.github.com>
2024-12-28 02:00:59 +0300

    Update MainForm.cs

--- MotionUI/View/Forms/MainForm.cs
+++ MotionUI/View/Forms/MainForm.cs
@@ -24,6 +24,7 @@ namespace MotionUI
             InitializeComponent();
             InitializeComponent();
             InitializeComponent();
+            InitializeComponent();
             Initialize();
             Initialize();
         }
