408c52e0b3c2e4ac4a9dc15b9dfd57089f5d80b1
Ahmet Yusuf Karaman <90053607+karamanyusufahmet@users.noreply.github.com>
2024-12-29 00:38:44 +0300

    Update Settings.Designer.cs

--- MotionUI/Properties/Settings.Designer.cs
+++ MotionUI/Properties/Settings.Designer.cs
@@ -19,6 +19,12 @@ namespace MotionUI.Properties
 
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         private static deneme;
+        private static deneme;
+        private static deneme;
+        private static deneme;
+        private static deneme;
+        private static deneme;
+        private static deneme;
         public static Settings Default
         {
             get
